34d5dfcdfedb483d634968a542876483573ee898
Lucien Lasseau <62194841+AlurienFlame@users.noreply.github.com>
2022-12-13 15:55:38 -0800

    Pause button

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ import { Board } from "./board.js";
 
 // setup
 let board = new Board();
-board.draw();
+let paused = true;
 
 // main loop
 let mainLoopInterval = setInterval(main, 1000);
 function main() {
+  if (paused) return;
   // TODO: Accelleration
   board.activeTetromino.move(0, 1);
   board.draw();
@@ -20,6 +21,12 @@ function main() {
 // Event listeners
 // TODO: check if key is down every few milliseconds instead of listening for keydown
 document.addEventListener("keydown", (event) => {
+  if (["p", "Escape"].includes(event.key)) {
+    togglePause();
+    return;
+  }
+  if (paused) return;
+
   switch (event.key) {
     case "ArrowLeft":
       board.activeTetromino.move(-1, 0);
@@ -35,11 +42,19 @@ document.addEventListener("keydown", (event) => {
       break;
     case " ":
       while (board.activeTetromino.move(0, 1)) {} // Move down until .move returns false
+      event.preventDefault(); // Prevent spacebar from pushing buttons
       break;
     case "c":
       board.hold();
       break;
-    // TODO: Add pause
   }
   board.draw();
 });
+
+// Pause button
+const pauseButton = document.getElementById("play-button");
+pauseButton.addEventListener("click", togglePause);
+function togglePause() {
+  paused = !paused;
+  pauseButton.innerText = paused ? "Play" : "Pause";
+}
